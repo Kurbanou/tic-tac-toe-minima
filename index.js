2f6f4f71c358c4f165bac0be6126fdaeaa488f79
Farid Kurbanou <kurbanou.faridun@gmail.com>
2022-03-10 23:05:20 +0300

    add

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const game = document.querySelector('.game')
 const result = document.querySelector('.result')
 const newGame = document.querySelector('.newGame')
-const AI = 'X', player = 'O'
+const ai = 'O', player = 'X'
 
 
 class Game {
@@ -9,47 +9,50 @@ class Game {
         this.size = size
         this.turn = Math.floor(Math.random() * 2)
         this.turnCount = 0
+
         newGame.addEventListener('click', () =>{
-            this.resetGame()   
+            this.resetGame()
         })
-        this.ceilList = []
-        this.resetGame()        
+
+        this.cellList = []
+        this.resetGame()
     }
 
     get limit() {
         return this.size * this.size
     }
 
-    init(){        
-        for(let i = 1; i < this.limit+1; i++){                                  
-            const ceil = document.createElement('div')
-            ceil.setAttribute('data-id', i-1)
-            ceil.classList.add('ceil')                      
-            game.append(ceil)           
-            ceil.addEventListener('click', this.humanPlay)    
-            this.ceilList.push(ceil)                                 
+    init(){
+        for(let i = 1; i < this.limit+1; i++){
+            const cell = document.createElement('div')
+            cell.setAttribute('data-id', i-1)
+            cell.classList.add('cell')
+            game.appendChild(cell)
+            cell.addEventListener('click', this.humanPlay())
+            this.cellList.push(cell)
             setTimeout(function(){
-                ceil.classList.add('show')
-            },`${9-i}00`)          
-        }       
+                cell.classList.add('show')
+            },`${9-i}00`)
+        }
     }
 
     resetGame(){
         this.board = [...Array(this.limit).keys()]
-        this.ceilList = []
-        this.turnCount = 0
         result.innerHTML = ''
         game.innerHTML = ''
+        this.turnCount = 0
+        this.cellList = []
         this.init()
     }
 
     humanPlay(){
-        return (e) => {
-            this.turnCount++
+        return e => {
+            this.turnCount += 1
             const id = e.target.getAttribute('data-id')
             this.board[+id] = player
-            this.ceilList[+id].innerHTML = `${player}`
-            if(this.turnCount === this.limit){
+            this.cellList[+id].innerHTML = `${player}`
+            this.cellList[+id].classList.add('active')
+            if(this.turnCount >= this.limit){
                 result.innerHTML = `Draw!`
                 return
             }
@@ -57,10 +60,27 @@ class Game {
                 result.innerHTML = `You win!`
                 return
             }
+            const bestMove = this.minimax(this.board, ai)
+            console.log(bestMove)
         }
     }
 
-    checkWinner(board,who){
+    makeAiTurn() {
+        this.turnCount += 1
+        const bestMove = this.minimax(this.board, ai)
+        this.board[bestMove.idx] = ai
+        this.cellList[bestMove.idx].innerHTML = `<span>${ai}</span>`
+        if (this.turnCount >= this.limit) {
+          result.innerHTML = `Draw!`
+          return
+        }
+        if (this.checkWinner(this.board, ai)) {
+          result.innerHTML = `AI win!`
+          return
+        }
+      }
+
+    checkWinner(board, who){
         if( board[0] === who && board[1] === who && board[2] === who ||
             board[3] === who && board[4] === who && board[5] === who ||
             board[6] === who && board[7] === who && board[8] === who ||
@@ -70,12 +90,73 @@ class Game {
             board[0] === who && board[4] === who && board[8] === who ||
             board[2] === who && board[4] === who && board[6] === who ){
             return true
-        }        
+        }
         return false
+    }
+
+    minimax(board, who){
+        const emptyCells = this.findEmptyCells(board)
+
+        if(this.checkWinner(board, player)){
+            return {score: -1}
+        }
+        if(this.checkWinner(board, ai)){
+            return {score: 1}
+        }
+
+        if(emptyCells.length === 0){
+            return {score: 0}
+        }
+
+        let moves = []
+
+        for(let i = 0; i < emptyCells.length; i++){
+            let move = {}
+            board[emptyCells[i]] = who
+            move.index = emptyCells[i]
+            if (who === player){
+                const res = this.minimax(board, ai)
+                move.score = res.score
+            }
+            if (who === ai){
+                const res = this.minimax(board, player)
+                move.score = res.score
+            }
+            board[emptyCells[i]] = move.index
+            moves.push(move)
+        }
+
+        let bestMove = null
+
+        if (who === ai) {
+            let bestScore = -Infinity
+            for (let i = 0; i < moves.length; i++) {
+              if (moves[i].score > bestScore) {
+                bestScore = moves[i].score
+                bestMove = i
+              }
+            }
+          } else {
+            let bestScore = Infinity
+            for (let i = 0; i < moves.length; i++) {
+              if (moves[i].score < bestScore) {
+                bestScore = moves[i].score
+                bestMove = i
+              }
+            }
+          }
+
+
+
+        return moves[bestMove]
+    }
+
+    findEmptyCells(board){
+        return board.filter(c => c !== ai && c !== player)
     }
 }
 
 new Game()
 
 
- 
+
